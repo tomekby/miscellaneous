f838a0439a4fd420370c7ec53dbc79206d4ed09b
Tomasz Stasiak <tomasz.stasiak@yahoo.pl>
2017-03-13 14:49:05 +0100

    Fixed benchmarks: - complexity should now be more properly calculated - switched to manual timing because PauseTiming and ResumeTiming is not working as intended - data put in benchmarks is now generated based on uniform distribution instead of previous rand() function - in reduce priority we count only really done operations Also added result of benchmarks

--- radix-heap/radix-heap/benchmarks.cpp
+++ radix-heap/radix-heap/benchmarks.cpp
@@ -2,67 +2,93 @@
 #include "radix.h"
 #include <ctime>
 #include <array>
+#include <chrono>
+#include <random>
+
+#define setIterationTime() auto end = std::chrono::high_resolution_clock::now(); \
+auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start); \
+state.SetIterationTime(elapsed_seconds.count());
+
+unsigned* getRandomInts(const unsigned count)
+{
+    // Inicjalizacja generatora liczb losowych
+    std::random_device r;
+    std::mt19937 gen(r());
+    std::uniform_int_distribution<> dis;
+
+    auto res = new unsigned[count];
+    for (auto i = 0; i < count; ++i)
+        res[i] = dis(gen);
+    return res;
+}
 
 void HeapPush(benchmark::State& state) {
-    srand(time(nullptr));
+    auto ints = getRandomInts(state.range(0));
 
-    RadixHeap<unsigned, unsigned> heap(state.range(0));
     while (state.KeepRunning()) {
-        auto x = rand() % state.range(0);
-        heap.push(x, x);
+        RadixHeap<unsigned, unsigned> heap(state.range(0));
+
+        auto start = std::chrono::high_resolution_clock::now();
+        for (auto i = 0; i < state.range(0); ++i)
+            heap.push(i, ints[i]);
+        setIterationTime()
     }
+    delete[] ints;
 
     state.SetLabel("Heap push");
-    state.SetComplexityN(state.range(0));
-    state.SetItemsProcessed(state.iterations());
+    state.SetComplexityN(state.iterations() * state.range(0));
+    state.SetItemsProcessed(state.iterations() * state.range(0));
 }
-BENCHMARK(HeapPush)->Range(32, 8 << 10)->Complexity();
+BENCHMARK(HeapPush)->Range(64, 8 << 16)->UseManualTime()->Complexity();
 
 void HeapPop(benchmark::State& state) {
-    // Filling of the queue shouldn't be benchmarked
-    const auto count = 1024 * 1024;
-    RadixHeap<unsigned, unsigned> heap(count);
-    for (auto i = 0; i < count; ++i) {
-        auto x = rand() % count;
-        heap.push(x, x);
-    }
+    auto ints = getRandomInts(state.range(0));
 
     while(state.KeepRunning()) {
-        benchmark::DoNotOptimize(heap.pop());
+        RadixHeap<unsigned, unsigned> heap(state.range(0));
+        for (auto i = 0; i < state.range(0); ++i) 
+            heap.push(i, ints[i]);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        for (auto i = 0; i < state.range(0); ++i)
+            benchmark::DoNotOptimize(heap.pop());
+        setIterationTime()
     }
+    delete[] ints;
 
     state.SetLabel("Heap pop");
-    state.SetComplexityN(state.range(0));
-    state.SetItemsProcessed(state.iterations());
+    state.SetComplexityN(state.iterations() * state.range(0));
+    state.SetItemsProcessed(state.iterations() * state.range(0));
 }
-BENCHMARK(HeapPop)->Range(32, 8 << 10)->Complexity();
+BENCHMARK(HeapPop)->Range(64, 8 << 16)->UseManualTime()->Complexity();
 
 void HeapReducePriority(benchmark::State& state) {
-    const auto count = 1024 * 1024;
-    // Filling of the queue shouldn't be benchmarked
-    RadixHeap<unsigned, unsigned> heap(count);
-    auto values = new unsigned[count];
-    auto minimum = count;
+    auto values = getRandomInts(state.range(0));
+    auto minimum = std::numeric_limits<unsigned>::max();
+    for (auto i = 0; i < state.range(0); ++i)
+        minimum = values[i] < minimum ? values[i] : minimum;
 
-    for (auto i = 0; i < count; ++i) {
-        auto x = rand() % count;
-        heap.push(x, x);
-        values[i] = x;
-        minimum = x < minimum ? x : minimum;
-    };
+    auto processed = 0;
+    while (state.KeepRunning()) {
+        RadixHeap<unsigned, unsigned> heap(state.range(0));
+        for (auto i = 0; i < state.range(0); ++i) 
+            heap.push(i, values[i]);
 
-    for (auto i = 0; state.KeepRunning(); i = i % count) {
-        if (values[i] > minimum) {
-            heap.reduce_priority(values[i], values[i] - 1);
+        auto start = std::chrono::high_resolution_clock::now();
+        for (auto i = 0; i < state.range(0); ++i) {
+            if (values[i] > minimum) {
+                heap.reduce_priority(i, values[i] -= 1);
+                ++processed;
+            }
         }
+        setIterationTime()
     }
+    delete[] values;
 
     state.SetLabel("Heap reduce priority");
-    state.SetComplexityN(state.range(0));
-    state.SetItemsProcessed(state.iterations());
-
-    delete[] values;
+    state.SetComplexityN(processed);
+    state.SetItemsProcessed(processed);
 }
-BENCHMARK(HeapReducePriority)->Range(32, 8 << 10)->Complexity();
+BENCHMARK(HeapReducePriority)->Range(64, 8 << 18)->UseManualTime()->Complexity();
 
 BENCHMARK_MAIN()
